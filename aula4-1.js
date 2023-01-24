aa211bad346a942c80aa3e11cfd8afed112609c8
Igor <igorsoaresr_contato@hotmail.com>
2023-01-24 16:45:41 -0300

    adiciona codigos aos arquivos

--- aula4-1.js
+++ aula4-1.js
@@ -1,3 +1,18 @@
-console.log('aula');
-console.log('aula');
-console.log('aula');
+let turma  = 12;
+let empresa = 'trybe';
+
+turma = 18;
+
+console.log(turma);
+
+num1 = 2;
+num2 = 3;
+
+let isEven = ((num1 + num2) % 0) == 0;
+console.log(isEven);
+
+let a = 0
+let b = "0"
+
+console.log(a === b); 
+console.log('acorda bb');
---
+++ code-runner.js
@@ -0,0 +1,3 @@
+console.log("Javascript é maravilhoso!");
+console.log("Javascript é maravilhoso!");
+console.log("Javascript é maravilhoso!");
