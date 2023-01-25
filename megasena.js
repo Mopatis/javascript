ec3e6756168e795159b23d088d89eefd94c01b43
Igor <igorsoaresr_contato@hotmail.com>
2023-01-25 13:07:12 -0300

    adicona checador de mega-sena

--- megasena.js
+++ megasena.js
@@ -9,4 +9,25 @@ let sixthNumber = Math.ceil(Math.random() * 60);
 
 //console.log(Math.random() * 60);
 megaSenaNumbers = [firstNumber, seconNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber];
+
+
+let turma12Play = [35, 8, 22, 14, 2, 60];
+
+let numberOfHits = 0;
+
+for (indexMega = 0; indexMega < megaSenaNumbers.length; indexMega += 1) {
+    for (let indexPlay = 0; indexPlay < turma12Play.length; indexPlay += 1) {
+        if (megaSenaNumbers[indexMega] === turma12Play[indexPlay]) {
+            numberOfHits += 1
+        };
+    };
+};
+if (numberOfHits === 0) {
+    console.log("Você não acertou nada!");
+} else {
+    console.log(numberOfHits);
+}
+
 console.log(megaSenaNumbers);
+console.log(turma12Play);
+ 
